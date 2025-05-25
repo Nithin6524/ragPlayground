39389dc7c664404009a539d74ef9467f6d95efbe
M SAI NITHIN <nithin6524@gmail.com>
2025-05-26 00:39:26 +0530

    added base url of the backend in the frontend

--- Frontend/lib/api.ts
+++ Frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000"; // Update to deployed URL later
+const API_BASE_URL = "https://ragplayground-production.up.railway.app/"; // Update to deployed URL later
 
 const api = axios.create({
     baseURL: API_BASE_URL,
