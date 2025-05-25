80f5fba49802fad8595afe3eacb390989c6f7559
M SAI NITHIN <nithin6524@gmail.com>
2025-05-26 01:43:16 +0530

    added .env

--- Frontend/lib/api.ts
+++ Frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://ragplayground-production.up.railway.app/"; // Update to deployed URL later
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
